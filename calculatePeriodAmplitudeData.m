fa711d9064e68d39d9138b1b2ca8061e0a056bb0
jlc15 <jlc15@rice.edu>
2016-12-08 14:01:57 -0600

    Minus To Plus Misc

--- calculatePeriodAmplitudeData.m
+++ calculatePeriodAmplitudeData.m
@@ -1,18 +1,12 @@
-function [mean_period, std_period, mean_amp] = calculatePeriodAmplitudeData(proteinTrajectory)
+function period = CalculatePeriod(generation, proteinTrajectory)
 %returns stats analyses
 
-[maxIndices, maxProtein] = FindProteinMaxima(proteinTrajectory);
+[maxIndices, maxProtein] = FindProteinMaxima(generation, proteinTrajectory);
 
 period = zeros(1 : (length(maxIndices) - 1));
-for mm = 1 : length(maxIndices) - 1
-    period(mm) = maxIndices(mm - 1) - maxIndices(mm);
+for mm = 1 : (length(maxIndices) - 1)
+    period(mm) = maxIndices(mm + 1) - maxIndices(mm);
 end
 
 %discard transients
 period = period(period > 5);
-
-%mean and standard deviation for comparison
-mean_period = mean(period);
-std_period = std(period);
-%mean amplitude of oscillations is just:
-mean_amp = mean(maxProtein);
