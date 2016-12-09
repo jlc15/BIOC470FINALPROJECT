e5d20a553fb06860b74d009b023cc5a873920d5c
malv-ika <mg55@rice.edu>
2016-12-08 20:38:59 -0600

    Update FindProteinMaxima.m

--- FindProteinMaxima.m
+++ FindProteinMaxima.m
@@ -1,7 +1,11 @@
-function [maxIndices, maxProtein] = FindProteinMaxima(generation, proteinTrajectory)
+function [maxIndices, maxProtein] = FindProteinMaxima(generation, proteinTrajectory, SpecifyDataOrModel)
 
 %find maxima of peaks of protein levels for a given trajectory
-[maxdat, maxidx] = extrema(smooth(proteinTrajectory, 750));
+if strcmp(SpecifyDataOrModel,'model') == 1
+    [maxdat, maxidx] = extrema(smooth(proteinTrajectory, 750));
+elseif strcmp(SpecifyDataOrModel,'data') == 1
+    [maxdat, maxidx] = extrema(proteinTrajectory);
+end
 maxima = maxdat(maxdat > mean(maxdat));
 maxima_idx = generation(maxidx(maxdat > mean(maxdat)));
 
